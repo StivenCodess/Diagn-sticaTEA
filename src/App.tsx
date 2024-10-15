33949afbb4af56fe5c28dbdc307a8bc25ee38e68
Matt <fabianbarzola2003@gmail.com>
2024-10-15 17:54:16 -0300

    feat: add tostify and review cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Nav from "./components/Nav/Nav";
 
+import "./index.css";
+import Home from "./Home/Home";
 function App() {
   return (
     <>
       <Nav />
+      <Home></Home>
     </>
   );
 }
--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,10 +1,11 @@
 import Agenda from "./Components/Agenda/Agenda";
+import Reviews from "./Components/Reviews/Reviews";
 export default function Home() {
   return (
     <div>
 
       <Agenda></Agenda>
-
+      <Reviews></Reviews>
     </div>
   )
 }
