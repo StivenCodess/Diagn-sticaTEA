2fbcf252a263845ce68a666f038a6dfb866260b6
Tiven <vairtor@gmail.com>
2024-10-16 18:08:00 -0300

    fix(home): refactor html and css

--- src/Sections/Home/Home.tsx
+++ src/Sections/Home/Home.tsx
@@ -1,11 +1,53 @@
-import Agenda from "./Components/Agenda/Agenda";
+import toast, { Toaster } from "react-hot-toast";
+import styles from "./Home.module.css";
+
 import Reviews from "./Components/Reviews/Reviews";
+
 export default function Home() {
   return (
-    <div>
+    <>
+      <section className={styles.section}>
+        <Toaster
+          position="top-center"
+          reverseOrder={false}
+          gutter={8}
+          containerClassName=""
+          containerStyle={{}}
+          toastOptions={{
+            className: "",
+            duration: 5000,
+            style: {
+              background: "#faf9f6",
+              color: "#2c2c2c",
+            },
+          }}
+        />
 
-      <Agenda></Agenda>
+        <main className={styles.container}>
+          <h2 className={styles.title}>
+            Evaluacion diagnostica complementaria
+          </h2>
+
+          <p className={styles.description}>
+            Entregamos herramientas a las familias y para mejorar la calidad de
+            vida de todos. Contamos con profesionales certificados.
+          </p>
+
+          <div className={styles.reserveContainer}>
+            <h4 className={styles.titleReserve}>
+              Agenda una hora para orientacion GRATUITA.
+            </h4>
+
+            <button
+              className={styles.buttonReserve}
+              onClick={() => toast.error("Las reservas no estan disponibles")}
+            >
+              Reservar turno
+            </button>
+          </div>
+        </main>
+      </section>
       <Reviews></Reviews>
-    </div>
-  )
+    </>
+  );
 }
