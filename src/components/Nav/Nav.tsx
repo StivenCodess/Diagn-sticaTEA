36ffac73a795e018652cdcb3e9b28f75df767a1e
Tiven <vairtor@gmail.com>
2024-10-21 08:24:15 -0300

    feat: add nav responsive

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,17 +1,46 @@
 import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faL, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 import styles from "./Nav.module.css";
 import logo from "../../assets/logo.png";
+import { useState } from "react";
 
 const Nav = () => {
+  const [closeNav, setCloseNav] = useState(true);
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.logoContainer}>
         <img src={logo} alt="Autism Logo" className={styles.logoImg} />
         <h1 className={styles.logoTitle}>DiagnosticaTEA</h1>
       </div>
+      <FontAwesomeIcon
+        icon={faBars}
+        onClick={() => setCloseNav(!closeNav)}
+        className={
+          !closeNav
+            ? `${styles.hamburguer}`
+            : `${styles.hamburguer} ${styles.hamburguerClosed}`
+        }
+      />
 
-      <nav className={styles.navContainer}>
+      <nav
+        className={
+          closeNav
+            ? `${styles.navContainer}`
+            : `${styles.navContainer} ${styles.closed}`
+        }
+      >
+        <div className={styles.closeIcon}>
+          <span>Menu</span>
+          <FontAwesomeIcon
+            icon={faXmark}
+            size="2xl"
+            onClick={() => setCloseNav(!closeNav)}
+          />
+        </div>
+
         <ul className={styles.ulContainer}>
           <li>
             <NavLink
